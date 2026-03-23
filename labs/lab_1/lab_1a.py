8ffa04ebbf9bfcd75d52a0c0d812bf8c7134584c
anti_m4tter <77402596+anti-m4ttter@users.noreply.github.com>
2026-03-23 13:44:50 -0500

    Add intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Matteo" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi guys! I'm Matteo, and I'm a sophomore in Corpus Christi, Texas. I loove and have loved astronomy and astrophysics since I could read, and after a few back and forths between that and aerospace, I'm pretty set on it! I plan on applying for CogWorks and Quantum Software at BWSI, mostly because when I was doing my (state-qualified, and I got 100$!) science fair project on developing a neural network to sort and rank exoplanets based on habitability I absolutely fell in love with data science. Other things about me is I'm the Science Bowl Co-Captain, next year's SciOly Captain, and am the supply officer for NJROTC! I also like jazz, aviation, Peanuts, and I adore The Hitchhiker's Guide to the Galaxy. I hope to make some new friends, and I'm excited to do this!! See y'all!")
 if __name__ == "__main__":
     main()
